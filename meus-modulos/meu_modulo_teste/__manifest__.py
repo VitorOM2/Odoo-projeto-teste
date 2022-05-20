b5ec0788787cf709b8ebbf09b9980cb3b21b8c0d
VitorOM2 <viktormarque2053@gmail.com>
2022-05-20 10:18:39 -0300

    [Novo] Meus modulos.
    Criado um diretório para a criação dos meus diretórios.

---
+++ meus-modulos/meu_modulo_teste/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Lliège
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Meu Modulo Teste',
+    'description': """
+        Meu modulo teste""",
+    'version': '13.0',
+    'license': 'AGPL-3',
+    'author': 'Lliège',
+    'website': 'www.Lliege.com.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
